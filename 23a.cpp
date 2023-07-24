1fe0b94c06ecf11a7635c60fd389ee439a3c30f8
CroxxN <saradgaihre19@gmail.com>
2023-07-24 20:26:54 +0545

    extreme spaghetti + file error need fix

---
+++ 23a.cpp
@@ -0,0 +1,16 @@
+
+/*
+User: crox-x
+LANG: C++                 
+Time: 1690209500
+*/
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}
+    
